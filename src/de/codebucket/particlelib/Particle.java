90697c0202ea233adca0a7a0036e3e294fe967a8
codebucketdev <divad.nnamtdeis@gmail.com>
2014-02-21 15:19:44 +0100

    Added new Particles

--- src/de/codebucket/particlelib/Particle.java
+++ src/de/codebucket/particlelib/Particle.java
@@ -7,6 +7,7 @@ public enum Particle
 	RAINBOW_SMOKE("reddust", 1.0F, 100), 
 	FIRE("flame", 0.05F, 100), 
 	HEART("heart",0.0F, 4), 
+	NOTE("note", 0.0F, 1),
 	MAGIC_RUNES("enchantmenttable", 1.0F, 100), 
 	LAVA_SPARK("lava", 0.0F, 4), 
 	SPLASH("splash", 1.0F, 40), 
@@ -28,7 +29,12 @@ public enum Particle
 	SPELL_AMBIENT("mobSpellAmbient", 1.0F, 100), 
 	VOID("townaura", 1.0F, 100),
 	BLOCK_BREAK("blockcrack", 0.1F, 100), 
-	BLOCK_DUST("blockdust", 0.1F, 100);
+	BLOCK_DUST("blockdust", 0.1F, 100),
+    FIREWORKS_SPARK("fireworksSpark", 0.0F, 50),
+    FOOTSTEP("footstep", 0.0F, 1),
+    LARGE_SMOKE("largesmoke", 0.0F, 1),
+    ICONCRACK("iconcrack_", 0.0F, 1),
+    TILECRACK("tilecrack_", 0.0F, 1);
 	
 	private String particleName;
 	private float defaultSpeed;
